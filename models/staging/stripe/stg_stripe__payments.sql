fcaeb11a519abfab8e4d2d4ceb05853a5fd762d9
Jason Benis <jason.benis@coastalcloud.us>
2026-09-10 10:17:29 -0400

    Create/update source files, redo staging models using dbt codegen package, add dbt_packages to gitignore

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,8 +1,20 @@
-select id as payment_id,
-    orderid as order_id,
-    paymentmethod as payment_method,
-    status,
-    amount / 100 as amount,
-    created as created_at
+with source as (
+
+    select * from {{ source('stripe', 'payment') }}
+
+),
+
+renamed as (
+    select id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        -- amount is stored in cents, convert it to dollars        
+        amount / 100 as amount,
+        created as created_at,
+        _batched_at as updated_at
     
-from raw.stripe.payment
+    from source
+)
+
+select * from renamed
